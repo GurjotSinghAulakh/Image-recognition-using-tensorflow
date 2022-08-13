8cacda9e9aade71a4df78b876db5f19919ee1ad5
mortazaBaqeri <s351899@oslomet.no>
2022-08-13 14:11:17 +0200

    +CSS
    added some styling to the website

---
+++ javascript1.js
@@ -0,0 +1,43 @@
+// Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
+let classifier;
+
+// A variable to hold the image we want to classify
+let img;
+
+let modelURL = 'https://teachablemachine.withgoogle.com/models/vih153yuj/';
+
+function preload() {
+    classifier = ml5.imageClassifier(modelURL + 'model.json');
+}
+
+function uploadImage(){
+  let image_object = document.getElementById("imageUploaded")
+  let image = URL.createObjectURL(image_object.files[0])
+  img = image;
+  document.getElementById("bildeKontainer").innerHTML = '<img src="'+img+'" class="product_img" />';
+
+  classifyVideo();
+}
+
+
+function setup() {
+  createCanvas(400, 400);  
+}
+
+function classifyVideo(){
+  img = loadImage(img);
+  classifier.predict(img, gotResult);
+}
+
+// A function to run when we get any errors and the results
+function gotResult(error, results) {
+  // Display error in the console
+  if (error) {
+    console.error(error);
+  } else {
+    // The results are in an array ordered by confidence.
+    console.log(results);
+    document.getElementById("classified_item").innerHTML = "Kategori:  " + results[0].label
+    document.getElementById("classified_confidence").innerHTML = "Sikkerhet: " + nf(results[0].confidence, 0, 2)
+  }
+}
