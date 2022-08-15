38c96d3e2483dcb4aabd55e4623e1cf2b894faab
mortazaBaqeri <s351899@oslomet.no>
2022-08-15 12:36:09 +0200

    Added hvitevarer.html/js
    We added hvitevarer.js/.html, and need to add the following for the next push:
    
    Add:
    Categories: -fryser, tørketrommel
    More data for the models
    Under-categories (stol-->kontorstol, lenestol etc)

--- hvitevarer.js
+++ hvitevarer.js
@@ -4,25 +4,25 @@ let classifier;
 // A variable to hold the image we want to classify
 let img;
 
-let modelURL = 'https://teachablemachine.withgoogle.com/models/vih153yuj/';
+
+let modelURL = 'https://teachablemachine.withgoogle.com/models/Em3j0Intk/';
 
 function preload() {
     classifier = ml5.imageClassifier(modelURL + 'model.json');
-    //img = loadImage('kontorstol.jpeg');
 }
 
 function uploadImage(){
   let image_object = document.getElementById("imageUploaded")
   let image = URL.createObjectURL(image_object.files[0])
   img = image;
-  document.getElementById("bildeKontainer").innerHTML = '<img src="'+img+'" />';
+  document.getElementById("bildeKontainer").innerHTML = '<img src="'+img+'" class="product_img" />';
 
   classifyVideo();
 }
 
 
 function setup() {
-  createCanvas(400, 400);  
+  createCanvas(300, 300);  
 }
 
 function classifyVideo(){
@@ -38,7 +38,7 @@ function gotResult(error, results) {
   } else {
     // The results are in an array ordered by confidence.
     console.log(results);
-    createDiv(`Label: ${results[0].label}`);
-    createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`);
+    document.getElementById("classified_item").innerHTML = "Category:  " + results[0].label + "\n";
+    document.getElementById("classified_confidence").innerHTML = "Confidence: " + nf(results[0].confidence, 0, 2);
   }
 }
