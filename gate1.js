055736a4ce49d57ad89dcb1a105e1ecb3255c81c
mortazaBaqeri <s351899@oslomet.no>
2022-09-17 16:28:56 +0200

    Added Gate 2
    Added gate 2, sofa. Made it easy to add more gates (undercategories) :)

--- gate1.js
+++ gate1.js
@@ -40,16 +40,92 @@ function loadFile(event) {
     })
 }
 
+class Gate2Sofa{
+    constructor(_sofaProbability){
+        console.log('We are '+_sofaProbability.toFixed(2)+' confident it is a Sofa');
+        this.modelURLSofa = "https://teachablemachine.withgoogle.com/models/eCG1GaW07/";
+        this.model;
+    }
+
+    predictSofa(){
+        this.init_Sofa().then(() => {
+            this.predict();
+        })
+    }
+
+    // metod
+    async init_Sofa() {
+
+        console.log("Sofa er initiert!")
+
+        // load the model and metadata
+        const modelURL = this.modelURLSofa + "model.json";
+        const metadataURL = this.modelURLSofa + "metadata.json";
+    
+        // load the model and metadata
+        model = await tmImage.load(modelURL, metadataURL);
+        this.maxPredictionsSofa = model.getTotalClasses();
+    
+        
+        labelContainer = document.getElementById("label-container");
+        for (let i = 0; i < this.maxPredictionsSofa; i++) { 
+            // and class labels
+            labelContainer.appendChild(document.createElement("div"));
+        }
+
+        console.log("Sofa er ferdig!")
+    }
+
+    async predict() {
+        console.log("Starter sofa-prediction...")
+        var image = document.getElementById('imagePreview');
+    
+        // predict can take in an image, video or canvas html element
+        const prediction = await model.predict(image, false);
+
+        console.log("sofa perdiction:")
+        console.log(prediction)
+
+        for (let i = 0; i < this.maxPredictionsSofa; i++) {
+            const classPrediction =
+                prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+            labelContainer.childNodes[i].innerHTML = classPrediction;
+        }
+    }
+}
+
+
 async function predict() {
     var image = document.getElementById('imagePreview');
 
     // predict can take in an image, video or canvas html element
     const prediction = await model.predict(image, false);
 
+    // finner størst sannsynlighet:
+    let storst_sannsynlight = 0;
+    let storst_sannsynlight_label = "";
+    
+
+    for (i=0; i<maxPredictions; i++){
+        if(prediction[i].probability > storst_sannsynlight){
+            storst_sannsynlight = prediction[i].probability;
+            storst_sannsynlight_label = prediction[i].className;
+        }
+    }
+
+    //console.log(storst_sannsynlight_label + " med sikkerhet på " + storst_sannsynlight + "%")
+    console.log(prediction)
+
     for (let i = 0; i < maxPredictions; i++) {
         const classPrediction =
             prediction[i].className + ": " + prediction[i].probability.toFixed(2);
         labelContainer.childNodes[i].innerHTML = classPrediction;
+    }
+
+    
+    if (storst_sannsynlight_label == "Sofa"){
+        let sofa = new Gate2Sofa(storst_sannsynlight);
+        sofa.predictSofa();
     }
 }
 
